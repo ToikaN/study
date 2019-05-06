5c84f91c7dba58d02cfa09559ed6edcf3b62dcb2
ToikaN <pipolufu@gmail.com>
2019-05-06 21:00:27 +0900

    JS オブジェクト
    オブジェクトの操作

--- JavaScirptBasics/js/main.js
+++ JavaScirptBasics/js/main.js
@@ -8,5 +8,16 @@
     score: 32,  //最後の行に,を付けとくと行の入れ替えや追加が楽
   };
 
+  console.log(player.name);
+  console.log(player["name"]);
+
+  player.score = 100;
   console.log(player);
+
+  player.email = "taguchi@gmail.com"; //上で宣言したものに新たに要素を追加することができる
+  console.log(player);
+
+  delete player.score;
+  console.log(player);
+  
 }
