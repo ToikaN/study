f995f08bb30f4143a40788511156e3e4ff831d66
ToikaN <pipolufu@gmail.com>
2019-05-06 22:31:50 +0900

    JS オブジェクト
    forEach

--- JavaScirptBasics/js/main.js
+++ JavaScirptBasics/js/main.js
@@ -1,11 +1,19 @@
 'use strict';
 
 {
-  const a = [1, 5 ,10]
-  a.splice(2, 0, 6, 7);
-  console.log(a); //1,5,6,7,10
+  const a = [1, 5 ,10, 100]
 
-  const removed = a.splice(1,2);
-  console.log(a); //1,7,10
-  console.log(removed);
+  for (let i = 0; i <= a.length; i++){
+    console.log(i);
+  }
+
+  a.forEach(item => {
+    console.log(item);
+  });
+
+  a.forEach((item,index) => {
+    console.log(`${index}:${item}`);
+  });
+
+
 }
