54b2ebe067855757d7f133b9971135dc07c4b57d
ToikaN <pipolufu@gmail.com>
2019-05-06 21:20:17 +0900

    JS オブジェクト
    メソッドについて

--- JavaScirptBasics/js/main.js
+++ JavaScirptBasics/js/main.js
@@ -8,8 +8,30 @@
       this.name = name;
       this.score = score;
     }
+
+    showInfo() {
+      console.log(`name: ${this.name} score: ${this.score}`);
+    }
+
+    static showVersion() {
+      console.log("Player class ver. 1.0");
+    }
   }
 
+  const taguchi = new Player("taguchi", 32)
+    // taguchiというオブジェクトを作ってると考える
+    // const taguchi = {
+    //   name: "taguchi"
+    //   score: 32;
+    // }
+  const fkoji = new Player("fkoji", 44)
+
+  console.log(taguchi.name);
+
+  taguchi.showInfo();
+  fkoji.showInfo();
+
+  Player.showVersion();
 
 
 }
