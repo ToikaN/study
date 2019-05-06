0db33ff57cb29bc549d4fc1b6fd39ef174909eb6
ToikaN <pipolufu@gmail.com>
2019-05-07 00:34:33 +0900

    MOD
    #10
    要素のコピー、削除

--- JavaScirptBasics/js/main.js
+++ JavaScirptBasics/js/main.js
@@ -2,10 +2,22 @@
 //Document Ovject Modle DOM
 {
 
+  const h1 = document.createElement('h1');
+    h1.textContent = 'Title';
+    document.body.appendChild(h1);
 
-const div = document.querySelector('div');
+    const p = document.createElement('p');
+    p.textContent = 'Hello, hello, hello...';
+    document.body.appendChild(p);
 
-div.className = 'box border-pink';
+    const h2 = document.createElement('h2');
+    h2.textContent = 'Sub Title';
+    document.body.insertBefore(h2, p);
 
+    // const copy = p.cloneNode(true);
+      const copy = p.cloneNode(false);
+      document.body.insertBefore(copy, h2);
 
+      document.body.removeChild(h2);
+      
 }
