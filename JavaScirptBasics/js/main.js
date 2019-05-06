a82da451fffc00c1aa9922b5e8d2f9b74b3cd81e
ToikaN <pipolufu@gmail.com>
2019-05-06 22:06:42 +0900

    JS オブジェクト
    クラス継承

--- JavaScirptBasics/js/main.js
+++ JavaScirptBasics/js/main.js
@@ -3,7 +3,7 @@
 {
   // クラス
 
-  class Player {
+  class Player { //親クラス
     constructor(name, score) { //メソッド
       this.name = name;
       this.score = score;
@@ -12,26 +12,21 @@
     showInfo() {
       console.log(`name: ${this.name} score: ${this.score}`);
     }
+  }
 
-    static showVersion() {
-      console.log("Player class ver. 1.0");
+  class SoccerPlayer extends Player { //子クラス
+    constructor(name, score, number){
+      super(name, score);
+      this.number = number;
+    }
+    kick() {
+      console.log('Gooooooooooaaaaaaaal!');
     }
   }
 
-  const taguchi = new Player("taguchi", 32)
-    // taguchiというオブジェクトを作ってると考える
-    // const taguchi = {
-    //   name: "taguchi"
-    //   score: 32;
-    // }
-  const fkoji = new Player("fkoji", 44)
+  const tsubasa = new SoccerPlayer('tsubasa' ,99, 10);
 
-  console.log(taguchi.name);
-
-  taguchi.showInfo();
-  fkoji.showInfo();
-
-  Player.showVersion();
-
-
+  tsubasa.kick();
+  console.log(tsubasa.number);
+  tsubasa.showInfo();
 }
