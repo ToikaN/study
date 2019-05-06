1400acc0923f6830b7e18c781cadba7a8168b0f1
ToikaN <pipolufu@gmail.com>
2019-05-06 20:37:22 +0900

    JS
    アロー関数

--- JavaScirptBasics/js/main.js
+++ JavaScirptBasics/js/main.js
@@ -148,10 +148,23 @@ console.log(price * 170);
 // console.log(total);
 
 //関数式
-function sum(a, b, c) {
-  return a + b + c
-};
+// function sum(a, b, c) {
+//   return a + b + c
+// };
+//
+// const total = sum(1, 2, 3) + sum(3, 4, 5);
+//
+// console.log(total);
 
-const total = sum(1, 2, 3) + sum(3, 4, 5);
+//アロー関数
 
-console.log(total);
+// const sum = (a, b, c) => {
+//   return a + b + c;
+// };
+// // const sum = (a, b, c) => a + b + c; // 一行でreturnするならこの書き方でOK
+// const total = sum(1, 2, 3) + sum(3, 4, 5);
+//
+// console.log(total);
+
+const double = a => a * 2; //変数1文字ならこのように書くことができる
+console.log(double(12));
