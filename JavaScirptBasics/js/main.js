dfeac683f8d8ccefa786f9036a8535dd22770965
ToikaN <pipolufu@gmail.com>
2019-05-06 20:50:08 +0900

    JS オブジェク
    配列

--- JavaScirptBasics/js/main.js
+++ JavaScirptBasics/js/main.js
@@ -4,5 +4,14 @@
   // 配列
 
   const scores = [80, 90, 40]
+  console.log(scores[1]);
+
+  scores[2] = 100;
   console.log(scores);
+
+  console.log(scores.length);
+
+  // constは再代入ができない
+  // しかし定義されたものが配列やオブジェクトなら
+  // 中身の変更をすることができる
 }
