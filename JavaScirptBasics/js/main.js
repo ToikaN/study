0441e244d76ffe4e62d767ec16ee5f5b8d16be06
ToikaN <pipolufu@gmail.com>
2019-05-06 19:00:53 +0900

    JS
    コンソールの文法
    使い方

--- JavaScirptBasics/js/main.js
+++ JavaScirptBasics/js/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-console.log('Hello world from main.js!');
+console.log('Hello\tworld \nfrom main.js!');
+
+console.log("it's me!");
+console.log('it\'s me!');
+// '',""は同じ意味。ソース内では統一することが望ましい。
+console.log("hello"+'world');
