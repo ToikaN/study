25d2d56baeeddce447a65f20e7030ffa7d5ed7d1
ToikaN <pipolufu@gmail.com>
2019-05-06 23:50:45 +0900

    DOM
    querSelector

--- JavaScirptBasics/js/main.js
+++ JavaScirptBasics/js/main.js
@@ -3,7 +3,11 @@
 {
 
 
-  document.getElementById('target').textContent = 'Changed!';
+  document.querySelector('h1').textContent ='H1!';
+
+  document.querySelectorAll('li:nth-child(odd)').forEach(li => {
+    li.textContent = 'li!';
+  })
 
 
 }
