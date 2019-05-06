579690664e5df2489d586ca4c4a78de3bd6c2b9d
ToikaN <pipolufu@gmail.com>
2019-05-06 19:15:08 +0900

    JS
    定数

--- JavaScirptBasics/js/main.js
+++ JavaScirptBasics/js/main.js
@@ -1,8 +1,28 @@
 'use strict';
-
+/*
 console.log('Hello\tworld \nfrom main.js!');
 
 console.log("it's me!");
 console.log('it\'s me!');
 // '',""は同じ意味。ソース内では統一することが望ましい。
 console.log("hello"+'world');
+*/
+/*
+console.log(10 + 3); // たす
+console.log(10 - 3); // ひく
+console.log(10 * 3); // かける
+console.log(10 / 3); // わる
+console.log(10 % 3); // わったあまり
+console.log(10 ** 3); // 10の三乗
+
+console.log(2 + 10 * 3); // 演算子の優先度は数学と同じ
+console.log((2 + 10) * 3);
+*/
+
+/*
+定数
+const price = 150;
+
+console.log(price * 150);
+console.log(price * 170);
+*/
