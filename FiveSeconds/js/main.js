62e46e6b86c0255634763b2f26f5f31a35493347
ToikaN <pipolufu@gmail.com>
2019-05-07 09:31:04 +0900

    5sec game
    #5
    経過時間を表示

--- FiveSeconds/js/main.js
+++ FiveSeconds/js/main.js
@@ -11,6 +11,10 @@
   });
 
   stop.addEventListener('click', function() {
-
+    var elapsedTime;
+    elapsedTime = (Date.now() - startTime) / 1000;
+    // elapsedTime = 4; //確認用
+    // result.textContent = elapsedTime; ↓で小数点以下の000を表す
+    result.textContent = elapsedTime.toFixed(3);
   });
 })();
