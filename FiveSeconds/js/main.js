f363066be28230f368a6d66f99ec47779a493036
ToikaN <pipolufu@gmail.com>
2019-05-07 09:36:02 +0900

    5sec game
    #6
    結果に応じてスタイルを変える

--- FiveSeconds/js/main.js
+++ FiveSeconds/js/main.js
@@ -12,9 +12,15 @@
 
   stop.addEventListener('click', function() {
     var elapsedTime;
+    var diff;
     elapsedTime = (Date.now() - startTime) / 1000;
     // elapsedTime = 4; //確認用
     // result.textContent = elapsedTime; ↓で小数点以下の000を表す
     result.textContent = elapsedTime.toFixed(3);
+    diff = elapsedTime - 5.0;
+    // if (diff > -1.0 && diff < 1.0) {
+    if (Math.abs(diff) < 1.0) { //Math.ads(diff)でdiffの絶対値をとる
+      result.className = 'perfect';
+    }
   });
 })();
